6310b80979650d8b4b22fae59ea8a00febf0b5fb
haisenheim <clementessomba@gmail.com>
2025-01-06 15:51:25 +0100

    evols

--- resources/views/Analyste/Dossiers/show.blade.php
+++ resources/views/Analyste/Dossiers/show.blade.php
@@ -453,7 +453,7 @@
             data: new FormData( this ),
             success:function(data){
                 console.log(data)
-                window.location.replace('/analyste/dossier/'+token)
+                window.location.replace('/analyste/dossiers/'+token)
             },
             processData: false,
             contentType: false
