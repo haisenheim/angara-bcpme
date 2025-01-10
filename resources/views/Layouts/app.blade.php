14efe9296b2ec807e201540ddd71fb5ddf638e75
haisenheim <clementessomba@gmail.com>
2025-01-10 12:06:12 +0100

    login page

--- resources/views/Layouts/app.blade.php
+++ resources/views/Layouts/app.blade.php
@@ -84,7 +84,7 @@
             <!-- FOOTER -->
             <footer class="content__boxed mt-auto">
                 <div class="content__wrap py-3 py-md-1 d-flex flex-column flex-md-row align-items-md-center">
-                    <div class="text-nowrap mb-4 mb-md-0">Copyright &copy; {{ date('Y') }} <a href="#" class="ms-1 btn-link fw-bold">Alliages Technologies - Adjuvant</a></div>
+                    <div class="text-nowrap mb-4 mb-md-0">Copyright &copy; {{ date('Y') }} <a href="#" class="ms-1 btn-link fw-bold text-anger">Angara finance</a></div>
                     <nav class="nav flex-column gap-1 flex-md-row gap-md-3 ms-md-auto" style="row-gap: 0 !important;">
                         <a class="nav-link px-0" href="#">Contactez nous</a>
                     </nav>
