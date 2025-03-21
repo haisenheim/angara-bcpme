d876d10a9a7a7fd54596a96a73e5950282796499
haisenheim <clementessomba@gmail.com>
2025-03-21 17:57:08 +0100

    evols

--- resources/views/Layouts/gestionnaire.blade.php
+++ resources/views/Layouts/gestionnaire.blade.php
@@ -217,12 +217,12 @@ $agence = \Illuminate\Support\Facades\Session::get('agence');
     <ul class="mainnav__menu nav flex-column">
        <li class="nav-item">
            <a href="{{ route('gestionnaire.cooperatives.index') }}" class="nav-link mininav-toggle {{ $active==2?'active':'' }}"><i class="pli-leafs fs-3 me-2"></i>
-               <span class="nav-label mininav-content ms-1">Coopératives</span>
+               <span class="nav-label mininav-content ms-1">Organisations intermédiaires</span>
            </a>
        </li>
        <li class="nav-item">
             <a href="{{ route('gestionnaire.wallets.index') }}" class="nav-link mininav-toggle {{ $active==2?'active':'' }}"><i class="pli-coins fs-3 me-2"></i>
-                <span class="nav-label mininav-content ms-1">Wallets de Coopératives</span>
+                <span class="nav-label mininav-content ms-1">Wallets</span>
             </a>
         </li>
         <li class="nav-item">
