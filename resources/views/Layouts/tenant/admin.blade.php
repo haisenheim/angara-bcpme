7def1555dff71733c180c9f96c7b5c4584d3adb8
haisenheim <alliages.technologies@gmail.com>
2025-09-03 14:51:24 +0100

    demo

--- resources/views/Layouts/tenant/admin.blade.php
+++ resources/views/Layouts/tenant/admin.blade.php
@@ -219,7 +219,7 @@
 
 
         <li class="nav-item">
-            <a href="{{ route('admin.reglements.index') }}" class="nav-link mininav-toggle {{ $active==18?'active':'' }}"><i class="pli-coins fs-3 me-2"></i>
+            <a href="#" class="nav-link mininav-toggle {{ $active==18?'active':'' }}"><i class="pli-coins fs-3 me-2"></i>
                 <span class="nav-label mininav-content ms-1">Paiements producteurs</span>
             </a>
         </li>
--- resources/views/Tenant/Admin/Paiements/index.blade.php
+++ resources/views/Tenant/Admin/Paiements/index.blade.php
@@ -22,40 +22,32 @@
 @section('content')
     <div class="card">
         <div class="card-body">
-            <table class="table table-sm table-bordered">
-                <thead>
-                    <tr>
-                        <th>DATE</th>
-                        <th>DATE DE PAIEMENT</th>
-                        <th>BANQUE</th>
-                        <th>ACHETEUR</th>
-                        <th>FACTURE</th>
-                        <th>QUANTITE</th>
-                        <th>TOTAL FACTURE</th>
-                        <th>MONTANT</th>
-                        <th>COMPTE</th>
-                        <th>JUSTIFICATIF</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($items as $p)
-                    <tr>
-                        <td>{{ $p->created_at->format('d/m/Y H:i')  }}</td>
-                        <td>{{ \Carbon\Carbon::parse($p->day)->format('d/m/Y')  }}</td>
-                        <td>{{ $p->banque->name  }}</td>
-                        <td>{{ $p->item->client->name }}</td>
-                        <td><a href="{{ route('admin.invoices.show',$p->item->token) }}">{{ $p->order->name }}</a></td>
-
-                        <th>{{ $p->item->quantity }} tonnes</th>
-                        <th>{{ number_format($p->item->total,0,',','.') }} FCFA</th>
-                        <th>{{ number_format($p->montant,0,',','.') }} FCFA</th>
-                        <td>{{ $p->compte }}</td>
-                        <td><a href="{{ $p->justificatif }}">cliquer ici</a></td>
-
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
+            <table class="table table-sm table-bordered table-striped table-hover">
+                            <thead>
+                                <tr>
+                                    <th>DATE</th>
+                                    <th>PRODUCTEUR</th>
+                                    <th>MODE PAIEMENT</th>
+                                    <th>MONTANT</th>
+                                    <th>CAISSE</th>
+                                    <th>WALLET</th>
+                                    <th>NUMERO CIBLE</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($paiements->reverse() as $pp)
+                                    <tr>
+                                        <td>{{ $pp->created_at->format('d/m/Y H:i') }}</td>
+                                        <td>{{ $pp->membre?->name}}</td>
+                                        <td>{{ $pp->mode?->name }}</td>
+                                        <th>{{ number_format($pp->montant,0,',','.') }} FCFA</th>
+                                        <td>{{ $pp->caisse?->name }}</td>
+                                        <td>{{ $pp->wallet?->name }}</td>
+                                        <td>{{ $pp->phone }}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
         </div>
     </div>
 @endsection
