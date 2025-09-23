c3cba10a34cb12b0168ba8451d563371a6e93fc6
haisenheim <alliages.technologies@gmail.com>
2025-09-23 15:15:13 +0100

    prod

--- resources/views/Layouts/tenant/admin.blade.php
+++ resources/views/Layouts/tenant/admin.blade.php
@@ -216,7 +216,7 @@
                 <span class="nav-label mininav-content ms-1">Mes Caisses</span>
             </a>
         </li>
-
+        
 
         <li class="nav-item">
             <a href="#" class="nav-link mininav-toggle {{ $active==18?'active':'' }}"><i class="pli-coins fs-3 me-2"></i>
