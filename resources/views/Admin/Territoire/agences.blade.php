6359835c820765aa9203a3b28c12b420a967cc0c
haisenheim <alliages.technologies@gmail.com>
2025-09-24 02:06:00 +0100

    villages

--- resources/views/Admin/Territoire/agences.blade.php
+++ resources/views/Admin/Territoire/agences.blade.php
@@ -31,26 +31,25 @@
                         <th>Actions</th>
                     </tr>
                 </thead>
-            </table>
-            <tbody>
-                @foreach($items as $item)
-                    <tr>
-                        <td>{{ $item->name }}</td>
-                        <td>{{ $item->representation?$item->representation->name:'-' }}</td>
-                        <td>
-                            <div class="btn-group">
-                                <button type="button" class="btn btn-xs btn-light dropdown-toggle hstack gap-3" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Actions
-                                    <span class="vr"></span>
-                                </button>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="{{ route('admin.agences.show',$item->id) }}"><i class="demo-pli-eye me-2 fs-5"></i> Voir</a></li>
-                                </ul>
-                            </div>
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
+                <tbody>
+                    @foreach($items as $item)
+                        <tr>
+                            <td>{{ $item->name }}</td>
+                            <td>{{ $item->representation?$item->representation->name:'-' }}</td>
+                            <td>
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-xs btn-light dropdown-toggle hstack gap-3" data-bs-toggle="dropdown" aria-expanded="false">
+                                        Actions
+                                        <span class="vr"></span>
+                                    </button>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item" href="{{ route('admin.agences.show',$item->id) }}"><i class="demo-pli-eye me-2 fs-5"></i> Voir</a></li>
+                                    </ul>
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
             </table>
         </div>
     </div>
